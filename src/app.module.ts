0e9465b155055f854674e2b08d7221dd3c53e0a9
evert2208 <65311634+evert2208@users.noreply.github.com>
2024-03-13 14:15:43 -0500

    variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroesVillanosModule } from './heroes-villanos/heroes-villanos.module';
 import { HeroesVillanos } from './heroes-villanos/entities/heroes-villano.entity';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'aws-0-us-west-1.pooler.supabase.com',
-      port: 5432,
-      username: 'postgres.vnmwutsnsyqwpgtgsdvu',
-      password: '4KrFFUaSxjkjyAJe',
-      database: 'postgres',
-      autoLoadModels: true,
-      synchronize: true,
-      models: [HeroesVillanos],
+    ConfigModule.forRoot({
+      isGlobal: true
     }),
+    SequelizeModule.forRootAsync({
+      useFactory: ()=>({
+        dialect: 'postgres',
+        host: process.env.HOST,
+        port: +process.env.PORT,
+        username: process.env.BD_USERNAME,
+        password: process.env.BD_CLAVE,
+        database: process.env.BD_NAME,
+        autoLoadModels: true,
+        synchronize: true,
+        models: [HeroesVillanos],
+      })
+     
+    }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: 'aws-0-us-west-1.pooler.supabase.com',
+    //   port: 5432,
+    //   username: 'postgres.vnmwutsnsyqwpgtgsdvu',
+    //   password: '4KrFFUaSxjkjyAJe',
+    //   database: 'postgres',
+    //   autoLoadModels: true,
+    //   synchronize: true,
+    //   models: [HeroesVillanos],
+    // }),
     HeroesVillanosModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+    console.log(process.env.HOST)}
+}
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,7 +13,9 @@ async function bootstrap() {
       validationError: { target: false }, 
     })
   );
-
-  await app.listen(3000);
+   
+  await app.listen(process.env.PORT_LOCAL,()=> {
+    console.log(`corriendo en puerto ${process.env.PORT_LOCAL}`)
+  });
 }
 bootstrap();
